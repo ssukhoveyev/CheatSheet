d5c6030b7796c615809f68010e706ccd1efd933f
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2021-09-07 16:55:08 +0300

    Add SettingsApp

---
+++ C#/SettingsApp/SettingsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Properties.Settings.Default.MyString = "111";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
