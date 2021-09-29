08aac36fe3474ac81579ba8e2c02d8c6e5974a6f
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2021-09-29 13:56:44 +0300

    Update

---
+++ C#/CompInfoApp/CompInfoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace CompInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "SELECT Manufacturer,Product, SerialNumber,Version FROM Win32_BaseBoard");
+            ManagementObjectCollection information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                    Console.WriteLine("{0} = {1}", data.Name, data.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
