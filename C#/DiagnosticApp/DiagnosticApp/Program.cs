d5683561cfedea1c2340b0f299c0ee236ddfc434
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2021-09-29 08:06:28 +0300

    Update

---
+++ C#/DiagnosticApp/DiagnosticApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiagnosticApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Thread.Sleep(1000);
+
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+            Console.ReadKey();
+        }
+    }
+}
