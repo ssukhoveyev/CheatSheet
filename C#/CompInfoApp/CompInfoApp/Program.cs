f278f65d6648ca9d5c1e4e111f67cdf4ba06f397
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2022-08-31 09:45:55 +0300

    Update

--- C#/CompInfoApp/CompInfoApp/Program.cs
+++ C#/CompInfoApp/CompInfoApp/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
+using System.Threading;
+using System.Diagnostics;
 
 namespace CompInfoApp
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //BaseBoard();
+
+            //CPUTime();
+
+            //Fan();
+
+            VideoController();
+
+            Console.ReadKey();
+        }
+
+        static void BaseBoard()
         {
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(
                 "SELECT Manufacturer,Product, SerialNumber,Version FROM Win32_BaseBoard");
@@ -19,8 +34,44 @@ namespace CompInfoApp
                 foreach (PropertyData data in obj.Properties)
                     Console.WriteLine("{0} = {1}", data.Name, data.Value);
             }
+        }
 
-            Console.ReadKey();
+        public static void CPUTime()
+        {
+            using (var pc = new PerformanceCounter("Processor", "% Processor Time", "_Total"))
+            {
+                int i = 0;
+                while (i < 4)
+                {
+                    Console.WriteLine(pc.NextValue());
+                    Thread.Sleep(1000);
+                    i++;
+                }
+            }
+        }
+
+        public static void Fan()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "SELECT DesiredSpeed FROM Win32_Fan");
+            ManagementObjectCollection information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                    Console.WriteLine("{0} = {1}", data.Name, data.Value);
+            }
+        }
+
+        public static void VideoController()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "SELECT Caption, AdapterRAM FROM Win32_VideoController");
+            ManagementObjectCollection information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                    Console.WriteLine("{0} = {1}", data.Name, data.Value);
+            }
         }
     }
 }
