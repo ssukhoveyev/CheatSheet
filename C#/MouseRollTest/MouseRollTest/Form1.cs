e94a4a0169f13cdad5afa226bf59d118339ee5ff
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2022-09-05 09:08:11 +0300

    Update

---
+++ C#/MouseRollTest/MouseRollTest/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Forms;
+
+namespace MouseRollTest
+{
+    public partial class Form1 : Form
+    {
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+
+            i = 50;
+
+            this.MouseWheel += new MouseEventHandler(this_MouseWheel);
+
+            progressBar1.Value = i;
+        }
+
+        void this_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                textBox1.Text = "Вверх" + e.Delta;
+                if(i < 100)
+                    progressBar1.Value = i++;
+            }
+            else
+            {
+                textBox1.Text = "Вниз" + e.Delta;
+                if(i > 0)
+                    progressBar1.Value = i--;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Middle)
+                MessageBox.Show("Click middle button.");
+        }
+    }
+}
---
+++ C#/SaveDataTableToXML/SaveDataTableToXML/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveDataTableToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable("table1");
+            dt.Columns.Add("name", typeof(string));
+            dt.Columns.Add("qu", typeof(int));
+
+            DataRow dr = dt.NewRow();
+            dr["name"] = "Имя 1";
+            dr["qu"] = 10;
+            dt.Rows.Add(dr);
+
+            DataRow dr1 = dt.NewRow();
+            dr1["name"] = "Имя 2";
+            dr1["qu"] = 20;
+            dt.Rows.Add(dr1);
+
+            StringWriter writer = new StringWriter();
+            dt.WriteXml(writer, XmlWriteMode.WriteSchema, false);
+
+            string str = writer.ToString();
+
+            Console.WriteLine(str);
+
+            //Загружаем таблицу из XML
+
+            DataTable dt2 = new DataTable();
+            byte[] bArr = Encoding.UTF8.GetBytes(str);
+            MemoryStream xmlStream = new MemoryStream(bArr);
+            dt2.ReadXml(xmlStream);
+
+            Console.WriteLine("Строк в загруженной таблице: " + dt2.Rows.Count);
+            foreach (DataRow drs in dt2.Rows)
+                Console.WriteLine($"{drs["name"]} - {drs["qu"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
