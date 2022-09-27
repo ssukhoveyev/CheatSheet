e2905f4cdfa79634e066311f785f992b0563511d
ssukhoveyev <52024567+ssukhoveyev@users.noreply.github.com>
2022-09-27 15:40:04 +0300

    Add LinqApp

---
+++ C#/LinqApp/LinqApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source.
+            int[] scores = { 97, 92, 81, 60 };
+
+            // Define the query expression.
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+            // Output: 97 92 81
+
+            //Сортировка
+            IEnumerable<int> highScoresQuery =
+            from score in scores
+            where score > 80
+            orderby score //descending
+            select score;
+
+            foreach (int i in highScoresQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            IEnumerable<string> highScoresQuery2 =
+            from score in scores
+            where score > 80
+            orderby score descending
+            select $"The score is {score}";
+
+            foreach (string i in highScoresQuery2)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+        }
+    }
+}
